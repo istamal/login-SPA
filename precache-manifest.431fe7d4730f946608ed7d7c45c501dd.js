6649009254de197bd296fc9ff36aaf58dc535669
istamal <dagalaev@gmail.com>
2020-05-10 02:09:30 +0300

    Updates

---
+++ precache-manifest.431fe7d4730f946608ed7d7c45c501dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15c4129ef26d2b45e3e2c1012b55b5a7",
+    "url": "/login-SPA/index.html"
+  },
+  {
+    "revision": "030bc077a93c4072a0a4",
+    "url": "/login-SPA/static/css/2.4b6e3ef2.chunk.css"
+  },
+  {
+    "revision": "a6b5d4f7d12bb3a7bbc3",
+    "url": "/login-SPA/static/css/main.ee7be533.chunk.css"
+  },
+  {
+    "revision": "030bc077a93c4072a0a4",
+    "url": "/login-SPA/static/js/2.61fbf169.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/login-SPA/static/js/2.61fbf169.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6b5d4f7d12bb3a7bbc3",
+    "url": "/login-SPA/static/js/main.cc7c0dd1.chunk.js"
+  },
+  {
+    "revision": "9fa381235b885b4abade",
+    "url": "/login-SPA/static/js/runtime-main.51098c9c.js"
+  }
+]);
